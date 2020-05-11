b8abf61088ef5ebe9b442b71feac1e60a8d430ba
Yara Atkinson <57329346+yaraatkinson@users.noreply.github.com>
2020-05-11 02:09:56 +0100

    Add files via upload

---
+++ Webtech Assignment Semester 2/Theatre Tickets/scripts/main.js
@@ -0,0 +1,37 @@
+function displayDetails() { 
+    const name = document.getElementById("name").value;
+    const address = document.getElementById("address").value;
+    const tickets = document.getElementById("tickets").value;
+    const optShow = document.getElementById("show");
+    const showOutput = document.getElementById("showResult");
+    const nameOutput = document.getElementById("nameResult");
+    const addressOutput = document.getElementById("addressResult");
+    const ticketsOutput = document.getElementById("ticketsResult");
+    const methodOutput = document.getElementById("methodResult");
+    const allMethods = document.querySelectorAll("input[name='method']");
+    const selectedMethod = 
+        document.querySelector("input[name='method']:checked");
+    
+    for (let i = 0; i < allMethods.length; i++) {
+        allMethods[i].addEventListener("change", checkPreference);
+    }
+    
+    nameOutput.innerText = "Name: " + name;
+    addressOutput.innerText = "Address: " + address;
+    ticketsOutput.innerText = "Number of tickets: " + tickets;
+
+    displaySelect();
+
+    function displaySelect() {
+    let show = optShow.options[optShow.selectedIndex].innerText;
+    showOutput.innerText = "Show: " + show;}
+
+   // const line4 = document.getElementById("showResult");
+    //let text4 = document.createTextNode(show);
+    
+    methodOutput.innerText = "Method: " + selectedMethod.value;
+
+    function checkPreference() {
+        methodOutput.innerText = "Method: " + selectedMethod.value;
+    }
+}
