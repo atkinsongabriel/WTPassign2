78903c2066d1a05cfc09e171e512668d8dc545f5
Yara Atkinson <57329346+yaraatkinson@users.noreply.github.com>
2020-05-11 22:22:00 +0100

    Add files via upload

--- Webtech Assignment Semester 2/Theatre Tickets/scripts/main.js
+++ Webtech Assignment Semester 2/Theatre Tickets/scripts/main.js
@@ -1,4 +1,7 @@
+// function that is run after clicking submit
 function displayDetails() { 
+
+    // displaying options chosen
     const name = document.getElementById("name").value;
     const address = document.getElementById("address").value;
     const tickets = document.getElementById("tickets").value;
@@ -8,7 +11,6 @@ function displayDetails() {
     const addressOutput = document.getElementById("addressResult");
     const ticketsOutput = document.getElementById("ticketsResult");
     const methodOutput = document.getElementById("methodResult");
-    const total = document.getElementById("total");
     const allMethods = document.querySelectorAll("input[name='method']");
     const selectedMethod = 
         document.querySelector("input[name='method']:checked");
@@ -20,40 +22,72 @@ function displayDetails() {
     nameOutput.innerText = "Name: " + name;
     addressOutput.innerText = "Address: " + address;
     ticketsOutput.innerText = "Number of tickets: " + tickets;
-
-    displaySelect();
-
-    function displaySelect() {
-    let show = optShow.options[optShow.selectedIndex].innerText;
-    showOutput.innerText = "Show: " + show;}
-    
     methodOutput.innerText = "Method: " + selectedMethod.value;
-
+    displaySelect();
+    function displaySelect() {
+        let show = optShow.options[optShow.selectedIndex].innerText;
+        showOutput.innerText = "Show: " + show;
+    }
     function checkPreference() {
         methodOutput.innerText = "Method: " + selectedMethod.value;
     }
 
-    calculateShow();
+    //end displaying options
 
-    function calculateShow() {
-           
-            total.innerText = 
-                show * tickets ;
+     //calculations
+     calculateMethodCost()
+
+     function calculateMethodCost() {
+         if (selectedMethod.value == "edelivery") {
+             methodCost = 0;
+         }
+ 
+         else if (selectedMethod.value == "boxoffice") {
+             methodCost = 1.50;
+         }
+ 
+         else if (selectedMethod.value == "posted") {
+             methodCost = 3.99;
+         }
         }
         
-    }
 
+     calculateShowCost()
+    
+     function calculateShowCost() {
+
+        if (show.value == "phantom") {
+            showCost = 79;
+        }
+        else if (show.value == "hamilton") {
+            showCost = 85;
+        }
+        else if (show.value == "lionking") {
+            showCost = 67;
+        }
+        else {
+            showCost = 83;
+        }
+
+        total.innerText = 
+            (parseFloat(showCost) * parseFloat(tickets)) + parseFloat(methodCost)
+
+    }
+    
+    //end calculations
+
+}
+//end submit sunction
+
+
+// function that is run after clicking clear, empties result box
 function clearDetails() {
-    const name = document.getElementById("name").value;
-    const address = document.getElementById("address").value;
-    const tickets = document.getElementById("tickets").value;
-    const optShow = document.getElementById("show");
+
     const showOutput = document.getElementById("showResult");
     const nameOutput = document.getElementById("nameResult");
     const addressOutput = document.getElementById("addressResult");
     const ticketsOutput = document.getElementById("ticketsResult");
     const methodOutput = document.getElementById("methodResult");
-    const allMethods = document.querySelectorAll("input[name='method']");
     
     nameOutput.innerText = "Name: ";
     addressOutput.innerText = "Address: " ;
@@ -61,3 +95,4 @@ function clearDetails() {
     methodOutput.innerText = "Method: "
     showOutput.innerText="Show: ";
 }
+//end clear function
